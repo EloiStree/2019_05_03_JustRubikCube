48bbc173d1a97408e1708055ba292fa687305b55
Eloi Stree <eloistree@gmail.com>
2025-08-23 15:58:00 +0200

    Save: Correct fake mouse code

--- Runtime/Script/Unstored/RubikMono_FakeMouseDown.cs
+++ Runtime/Script/Unstored/RubikMono_FakeMouseDown.cs
@@ -1,36 +1,92 @@
+using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
-public class ClickHandler : MonoBehaviour
+public class RubikMono_FakeMouseDown : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public Camera mainCamera;
-    public InputAction clickAction;
+    public Camera m_mainCamera;
+    public InputAction m_clickAction;
+    public InputAction m_clickPosition;
+    public bool m_passthrough; // Toggle to pass through all objects
+    public bool m_useOnDown = true; // Toggle for OnMouseDown event
+    public bool m_useOnUp = true; // Toggle for OnMouseUp event
 
     void OnEnable()
     {
-        clickAction.Enable();
-        clickAction.performed += OnClick;
+        m_clickAction.Enable();
+        m_clickPosition.Enable();
+        m_clickAction.performed += OnClick;
+        m_clickAction.canceled += OnClick;
     }
 
     void OnDisable()
     {
-        clickAction.performed -= OnClick;
-        clickAction.Disable();
+        m_clickAction.performed -= OnClick;
+        m_clickAction.canceled -= OnClick;
+        m_clickAction.Disable();
+        m_clickPosition.Disable();
     }
 
     private void OnClick(InputAction.CallbackContext ctx)
     {
-        if(mainCamera ==null)
-            mainCamera = Camera.main;
+        if (m_mainCamera == null)
+            m_mainCamera = Camera.main;
 
-        Vector2 screenPosition = Mouse.current.position.ReadValue();
-        Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+        Vector2 screenPosition = m_clickPosition.ReadValue<Vector2>();
+        Ray ray = m_mainCamera.ScreenPointToRay(screenPosition);
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if (m_passthrough)
         {
-            Debug.Log("Clicked on: " + hit.collider.name);
+            // Raycast through all objects
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            foreach (RaycastHit hit in hits)
+            {
+                ProcessHit(hit, ctx);
+            }
+        }
+        else
+        {
+            // Single raycast
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                ProcessHit(hit, ctx);
+            }
+        }
+    }
+
+    private void ProcessHit(RaycastHit hit, InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Clicked on: " + hit.collider.name);
+
+        // Check if the action is performed (mouse down) or canceled (mouse up)
+        if (ctx.performed && m_useOnDown)
+        {
             hit.collider.gameObject.SendMessage("OnMouseDown", SendMessageOptions.DontRequireReceiver);
+            ExecuteEvents.Execute<IPointerDownHandler>(hit.collider.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+        }
+        else if (ctx.canceled && m_useOnUp)
+        {
             hit.collider.gameObject.SendMessage("OnMouseUp", SendMessageOptions.DontRequireReceiver);
+            ExecuteEvents.Execute<IPointerUpHandler>(hit.collider.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
+        }
+    }
+
+    // Implement IPointerDownHandler
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (m_useOnDown)
+        {
+            gameObject.SendMessage("OnMouseDown", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    // Implement IPointerUpHandler
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (m_useOnUp)
+        {
+            gameObject.SendMessage("OnMouseUp", SendMessageOptions.DontRequireReceiver);
         }
     }
 }
